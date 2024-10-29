f2846d45df17ef6b95576971917ff7d398f92c3e
giov8 <giovani.ggm@gmail.com>
2024-10-29 16:25:58 -0300

    verificar login

--- app.py
+++ app.py
@@ -17,6 +17,25 @@ def home():
 def login():
     return render_template('login.html')
 
+# VERFIFICAR O LOGIN
+@app.route('/verificar-login', methods=['POST'])
+def verificar_login():
+# Pegando o que o usuário digitou no campo de entrada de user e senha
+    username = request.form['username']
+    password = request.form['password']
+
+    # Verifica se o usuario digitado está na lista e se 
+    # a senha está certa
+    if username in usuarios and usuarios[username] == password:
+        return f"Bem-vindo, {username}!"
+    else:
+        return "Usuário ou senha inválidos."
+
+
+
+
+
+
 # parte principal do
 if __name__ == '__main__':
     app.run(debug=True)
